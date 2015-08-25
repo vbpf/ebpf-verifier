4208d490950f9cbb27b4b66292e2ae102ac10a92
Rich Lane <rlane@bigswitch.com>
2015-08-25 16:53:27 -0700

    vm: implement more ALU opcodes

--- vm/ubpf_vm.c
+++ vm/ubpf_vm.c
@@ -113,6 +113,86 @@ ubpf_exec(const struct ubpf_vm *vm, void *ctx)
             reg[inst.dst] += reg[inst.src];
             reg[inst.dst] &= UINT32_MAX;
             break;
+        case EBPF_OP_SUB_IMM:
+            reg[inst.dst] -= inst.imm;
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_SUB_REG:
+            reg[inst.dst] -= reg[inst.src];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_MUL_IMM:
+            reg[inst.dst] *= inst.imm;
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_MUL_REG:
+            reg[inst.dst] *= reg[inst.src];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_DIV_IMM:
+            /* TODO catch division by zero */
+            reg[inst.dst] /= inst.imm;
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_DIV_REG:
+            /* TODO catch division by zero */
+            reg[inst.dst] /= reg[inst.src];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_OR_IMM:
+            reg[inst.dst] |= inst.imm;
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_OR_REG:
+            reg[inst.dst] |= reg[inst.src];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_AND_IMM:
+            reg[inst.dst] &= inst.imm;
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_AND_REG:
+            reg[inst.dst] &= reg[inst.src];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_LSH_IMM:
+            reg[inst.dst] <<= inst.imm;
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_LSH_REG:
+            reg[inst.dst] <<= reg[inst.src];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_RSH_IMM:
+            reg[inst.dst] >>= inst.imm;
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_RSH_REG:
+            reg[inst.dst] >>= reg[inst.src];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_NEG:
+            reg[inst.dst] = -reg[inst.dst];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_MOD_IMM:
+            /* TODO catch division by zero */
+            reg[inst.dst] %= inst.imm;
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_MOD_REG:
+            /* TODO catch division by zero */
+            reg[inst.dst] %= reg[inst.src];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_XOR_IMM:
+            reg[inst.dst] ^= inst.imm;
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_XOR_REG:
+            reg[inst.dst] ^= reg[inst.src];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
         case EBPF_OP_MOV_IMM:
             reg[inst.dst] = inst.imm;
             reg[inst.dst] &= UINT32_MAX;
@@ -121,6 +201,20 @@ ubpf_exec(const struct ubpf_vm *vm, void *ctx)
             reg[inst.dst] = reg[inst.src];
             reg[inst.dst] &= UINT32_MAX;
             break;
+        case EBPF_OP_ARSH_IMM:
+            reg[inst.dst] = (int32_t)reg[inst.dst] >> inst.imm;
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        case EBPF_OP_ARSH_REG:
+            reg[inst.dst] = (int32_t)reg[inst.dst] >> reg[inst.src];
+            reg[inst.dst] &= UINT32_MAX;
+            break;
+        /* TODO endian opcodes */
+
+        /* TODO ALU64 opcodes */
+
+        /* TODO MEM opcodes */
+
         case EBPF_OP_JGT_IMM:
             if (reg[inst.dst] > (uint32_t)inst.imm) {
                 pc += inst.offset;
@@ -143,6 +237,9 @@ ubpf_exec(const struct ubpf_vm *vm, void *ctx)
             break;
         case EBPF_OP_EXIT:
             return reg[0];
+
+        /* TODO remaining JMP opcodes */
+
         default:
             /* Should have been caught by validate() */
             fprintf(stderr, "internal uBPF error: unknown opcode 0x%02x at PC %u\n", inst.opcode, cur_pc);
@@ -166,10 +263,33 @@ validate(const struct ebpf_inst *insts, uint32_t num_insts, char **errmsg)
     for (i = 0; i < num_insts; i++) {
         struct ebpf_inst inst = insts[i];
         switch (inst.opcode) {
-        case EBPF_OP_ADD_REG:
         case EBPF_OP_ADD_IMM:
-        case EBPF_OP_MOV_REG:
+        case EBPF_OP_ADD_REG:
+        case EBPF_OP_SUB_IMM:
+        case EBPF_OP_SUB_REG:
+        case EBPF_OP_MUL_IMM:
+        case EBPF_OP_MUL_REG:
+        case EBPF_OP_DIV_IMM:
+        case EBPF_OP_DIV_REG:
+        case EBPF_OP_OR_IMM:
+        case EBPF_OP_OR_REG:
+        case EBPF_OP_AND_IMM:
+        case EBPF_OP_AND_REG:
+        case EBPF_OP_LSH_IMM:
+        case EBPF_OP_LSH_REG:
+        case EBPF_OP_RSH_IMM:
+        case EBPF_OP_RSH_REG:
+        case EBPF_OP_NEG:
+        case EBPF_OP_MOD_IMM:
+        case EBPF_OP_MOD_REG:
+        case EBPF_OP_XOR_IMM:
+        case EBPF_OP_XOR_REG:
         case EBPF_OP_MOV_IMM:
+        case EBPF_OP_MOV_REG:
+        case EBPF_OP_ARSH_IMM:
+        case EBPF_OP_ARSH_REG:
+            break;
+
         case EBPF_OP_JGE_REG:
         case EBPF_OP_JGE_IMM:
         case EBPF_OP_JGT_REG:
