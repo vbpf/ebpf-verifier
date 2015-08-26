56298cc2bfadfd4ffe88189442642daa5f7fc2b2
Rich Lane <rlane@bigswitch.com>
2015-08-26 11:09:03 -0700

    vm: add --mem option to test program

--- vm/test.c
+++ vm/test.c
@@ -20,13 +20,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <getopt.h>
+#include <errno.h>
 #include "ubpf.h"
+
+static void *readfile(const char *path, size_t maxlen, size_t *len);
 
 static void usage(const char *name)
 {
-    fprintf(stderr, "usage: %s [-h] [-a|--arg INT] BINARY\n", name);
+    fprintf(stderr, "usage: %s [-h] [-a|--arg INT] [-m|--mem PATH] BINARY\n", name);
     fprintf(stderr, "\nExecutes the eBPF code in BINARY and prints the result to stdout.\n");
-    fprintf(stderr, "If ARG is given it will be passed in r1. Otherwise r1 will be zero.\n");
+    fprintf(stderr, "If --arg is given then it will be passed in r1. Otherwise r1 will be zero.\n");
+    fprintf(stderr, "If --mem is given then the specified file will be read and a pointer\nto its data passed in r1.\n");
 }
 
 int main(int argc, char **argv)
@@ -34,12 +38,14 @@ int main(int argc, char **argv)
     struct option longopts[] = {
         { .name = "help", .val = 'h', },
         { .name = "arg", .val = 'a', .has_arg=1 },
+        { .name = "mem", .val = 'm', .has_arg=1 },
     };
 
     uint64_t arg = 0;
+    const char *mem_filename = NULL;
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "ha:", longopts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "ha:m:", longopts, NULL)) != -1) {
         switch (opt) {
         case 'a': {
             char *endptr;
@@ -50,6 +56,9 @@ int main(int argc, char **argv)
             }
             break;
         }
+        case 'm':
+            mem_filename = optarg;
+            break;
         case 'h':
             usage(argv[0]);
             return 0;
@@ -64,34 +73,29 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    const char *filename = argv[optind];
-    FILE *file;
-    if (!strcmp(filename, "-")) {
-        file = fdopen(STDIN_FILENO, "r");
-    } else {
-        file = fopen(filename, "r");
-    }
-
-    if (file == NULL) {
-        perror("fopen");
+    const char *code_filename = argv[optind];
+    size_t code_len;
+    void *code = readfile(code_filename, 65536*8, &code_len);
+    if (code == NULL) {
         return 1;
     }
 
-    int maxlen = 65536*8;
-    void *code = malloc(maxlen);
-    int offset = 0;
-    int rv;
-    while ((rv = fread(code+offset, 1, maxlen-offset, file)) > 0) {
-        offset += rv;
-    }
+    if (mem_filename != NULL) {
+        if (arg != 0) {
+            fprintf(stderr, "Can't specify both --arg and --mem\n");
+            return 1;
+        }
 
-    if (ferror(file)) {
-        perror("fread");
-        return 1;
+        void *mem = readfile(mem_filename, 1024*1024, NULL);
+        if (mem == NULL) {
+            return 1;
+        }
+
+        arg = (uintptr_t)mem;
     }
 
     char *errmsg;
-    struct ubpf_vm *vm = ubpf_create(code, offset, &errmsg);
+    struct ubpf_vm *vm = ubpf_create(code, code_len, &errmsg);
     if (vm == NULL) {
         fprintf(stderr, "Failed to create VM: %s\n", errmsg);
         free(errmsg);
@@ -104,4 +108,47 @@ int main(int argc, char **argv)
     ubpf_destroy(vm);
 
     return 0;
+}
+
+static void *readfile(const char *path, size_t maxlen, size_t *len)
+{
+    FILE *file;
+    if (!strcmp(path, "-")) {
+        file = fdopen(STDIN_FILENO, "r");
+    } else {
+        file = fopen(path, "r");
+    }
+
+    if (file == NULL) {
+        fprintf(stderr, "Failed to open %s: %s", path, strerror(errno));
+        return NULL;
+    }
+
+    void *data = calloc(maxlen, 1);
+    size_t offset = 0;
+    size_t rv;
+    while ((rv = fread(data+offset, 1, maxlen-offset, file)) > 0) {
+        offset += rv;
+    }
+
+    if (ferror(file)) {
+        fprintf(stderr, "Failed to read %s: %s", path, strerror(errno));
+        fclose(file);
+        free(data);
+        return NULL;
+    }
+
+    if (!feof(file)) {
+        fprintf(stderr, "Failed to read %s because it is too large (max %u bytes)\n",
+                path, (unsigned)maxlen);
+        fclose(file);
+        free(data);
+        return NULL;
+    }
+
+    fclose(file);
+    if (len) {
+        *len = offset;
+    }
+    return data;
 }
