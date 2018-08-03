86921f17b7f6cdade4bf1b1430d3a4fb8113524f
Elazar Gershuni <elazarg@gmail.com>
2018-08-02 20:52:15 -0700

    remove dated comment

--- vm/verifier.hpp
+++ vm/verifier.hpp
@@ -10,10 +10,6 @@ using std::string;
 using std::map;
 using std::vector;
 
-/*** Interface to the loader.
- true if valid; *errmsg will point to NULL
- false if invalid; *errmsg will point to a heap-allocated error message
-*/
 bool abs_validate(vector<struct ebpf_inst> insts,
                   string domain_name, enum ebpf_prog_type prog_type);
 
