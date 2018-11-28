05926b2969b65cf10b393dd0a6f4c67546f47c0e
Elazar Gershuni <elazarg@gmail.com>
2018-11-28 13:18:39 +0200

    minor syntax change

--- src/asm_dis.cpp
+++ src/asm_dis.cpp
@@ -4,7 +4,7 @@
 
 #include "asm.hpp"
 
-static Mem::Op getMemOp(uint8_t opcode) {
+static auto getMemOp(uint8_t opcode) -> Mem::Op {
     switch (opcode & EBPF_CLS_MASK) {
         case EBPF_CLS_ST : return Mem::Op::ST;
         case EBPF_CLS_STX: return Mem::Op::ST;
@@ -14,7 +14,7 @@ static Mem::Op getMemOp(uint8_t opcode) {
     return {};
 }
 
-static Width getMemWidth(uint8_t opcode) {
+static auto getMemWidth(uint8_t opcode) -> Width {
     switch (opcode & EBPF_SIZE_MASK) {
         case EBPF_SIZE_B : return Width::B;
         case EBPF_SIZE_H : return Width::H;
@@ -24,7 +24,7 @@ static Width getMemWidth(uint8_t opcode) {
 	return {};
 }
 
-static bool getMemX(uint8_t opcode) {
+static auto getMemX(uint8_t opcode) -> bool {
     switch (opcode & EBPF_CLS_MASK) {
         case EBPF_CLS_LD : return false; 
         case EBPF_CLS_ST : return false;
@@ -34,7 +34,7 @@ static bool getMemX(uint8_t opcode) {
     return {};
 }
 
-static Bin::Op getBinOp(uint8_t opcode) {
+static auto getBinOp(uint8_t opcode) -> Bin::Op {
     switch ((opcode >> 4) & 0xF) {
         case 0x0 : return Bin::Op::ADD;
         case 0x1 : return Bin::Op::SUB;
@@ -50,17 +50,17 @@ static Bin::Op getBinOp(uint8_t opcode) {
         case 0xb : return Bin::Op::MOV;
         case 0xc: return Bin::Op::ARSH;
     }
-    return Bin::Op::ARSH;
+    return {};
 }
 
-static Value getBinValue(ebpf_inst inst) {
+static auto getBinValue(ebpf_inst inst) -> Value {
     if (inst.opcode & EBPF_SRC_REG)
         return Reg{inst.src};
     else
         return Imm{inst.imm};
 }
 
-static Un::Op getUnOp(uint8_t opcode) {
+static auto getUnOp(uint8_t opcode) -> Un::Op {
     switch (opcode) {
         case EBPF_OP_NEG: return Un::Op::NEG;
         case EBPF_OP_LE : return Un::Op::LE;
@@ -69,7 +69,7 @@ static Un::Op getUnOp(uint8_t opcode) {
     return {};
 }
 
-static Jmp::Op getJmpOp(uint8_t opcode) {
+static auto getJmpOp(uint8_t opcode) -> Jmp::Op {
     switch (opcode | EBPF_SRC_REG) {
         case EBPF_OP_JEQ_REG : return Jmp::Op::EQ;
         case EBPF_OP_JGT_REG : return Jmp::Op::GT;
@@ -86,7 +86,52 @@ static Jmp::Op getJmpOp(uint8_t opcode) {
     return {};
 }
 
-static Instruction toasm(ebpf_inst inst, int32_t next_imm) {
+static auto makeMemOp(ebpf_inst inst) -> Instruction {
+    auto width = getMemWidth(inst.opcode);
+    auto mode = inst.opcode & EBPF_MODE_MASK;
+    switch (mode) {
+        case EBPF_MODE_MEM: {
+            auto op = getMemOp(inst.opcode);
+            bool isLoad = op == Mem::Op::LD;
+            return Mem{ 
+                .op = op,
+                .width = width,
+                .valreg = Reg{isLoad ? inst.dst : inst.src},
+                .basereg = Reg{isLoad ? inst.src : inst.dst},
+                .offset = (inst.opcode & 1) ? (Target)Offset{inst.offset} : Reg{inst.imm},
+            };
+        }
+        case EBPF_MODE_ABS: return Packet{width, inst.imm, {} };
+        case EBPF_MODE_IND: return Packet{width, inst.imm, Reg{inst.src} };
+        case EBPF_MODE_LEN: return {};
+        case EBPF_MODE_MSH: return {};
+        case EBPF_XADD: return LockAdd {
+            .width = width,
+            .valreg = Reg{inst.src},
+            .basereg = Reg{inst.dst},
+            .offset = inst.offset,
+        };
+    }
+    return Undefined{inst.opcode};
+}
+
+static auto makeAluOp(ebpf_inst inst) -> Instruction {
+    if (inst.opcode == EBPF_OP_NEG || inst.opcode == EBPF_OP_BE || inst.opcode == EBPF_OP_LE) {
+        return Un{ 
+            .op = getUnOp(inst.opcode),
+            .dst=inst.dst 
+        };
+    } else {
+        return Bin{ 
+            .op = getBinOp(inst.opcode), 
+            .is64 = (inst.opcode & EBPF_CLS_MASK) == EBPF_CLS_ALU64,
+            .dst = Reg{ inst.dst },
+            .v = getBinValue(inst),
+        };
+    }
+}
+
+static auto toasm(ebpf_inst inst, int32_t next_imm) -> Instruction {
     if (inst.opcode == EBPF_OP_LDDW_IMM) {
         uint64_t imm = (uint64_t(next_imm) << 32 | (uint32_t)inst.imm);
         return Bin{
@@ -98,49 +143,12 @@ static Instruction toasm(ebpf_inst inst, int32_t next_imm) {
     }
     switch (inst.opcode & EBPF_CLS_MASK) {
         case EBPF_CLS_LD: case EBPF_CLS_LDX:
-        case EBPF_CLS_ST: case EBPF_CLS_STX: {
-            auto width = getMemWidth(inst.opcode);
-            auto mode = inst.opcode & EBPF_MODE_MASK;
-            switch (mode) {
-                case EBPF_MODE_MEM: {
-                    auto op = getMemOp(inst.opcode);
-                    bool isLoad = op == Mem::Op::LD;
-                    return Mem{ 
-                        .op = op,
-                        .width = width,
-                        .valreg = Reg{isLoad ? inst.dst : inst.src},
-                        .basereg = Reg{isLoad ? inst.src : inst.dst},
-                        .offset = (inst.opcode & 1) ? (Target)Offset{inst.offset} : Reg{inst.imm},
-                    };
-                }
-                case EBPF_MODE_ABS: return Packet{width, inst.imm, {} };
-                case EBPF_MODE_IND: return Packet{width, inst.imm, Reg{inst.src} };
-                case EBPF_MODE_LEN: return {};
-                case EBPF_MODE_MSH: return {};
-                case EBPF_XADD: return LockAdd {
-                    .width = width,
-                    .valreg = Reg{inst.src},
-                    .basereg = Reg{inst.dst},
-                    .offset = inst.offset,
-                };
-            }
-            return Undefined{inst.opcode};
-        }
+        case EBPF_CLS_ST: case EBPF_CLS_STX:
+            return makeMemOp(inst);
+
         case EBPF_CLS_ALU:
         case EBPF_CLS_ALU64: 
-            if (inst.opcode == EBPF_OP_NEG || inst.opcode == EBPF_OP_BE || inst.opcode == EBPF_OP_LE) {
-                return Un{ 
-                    .op = getUnOp(inst.opcode),
-                    .dst=inst.dst 
-                };
-            } else {
-                return Bin{ 
-                    .op = getBinOp(inst.opcode), 
-                    .is64 = (inst.opcode & EBPF_CLS_MASK) == EBPF_CLS_ALU64,
-                    .dst = Reg{ inst.dst },
-                    .v = getBinValue(inst),
-                };
-            }
+            return makeAluOp(inst);
 
         case EBPF_CLS_JMP: 
             if (inst.opcode == EBPF_OP_JA) return Goto{ inst.offset };
@@ -157,6 +165,6 @@ static Instruction toasm(ebpf_inst inst, int32_t next_imm) {
     return {};
 }
 
-IndexedInstruction toasm(uint16_t pc, ebpf_inst inst, int32_t next_imm) {
+auto toasm(uint16_t pc, ebpf_inst inst, int32_t next_imm) -> IndexedInstruction {
     return {pc, toasm(inst, next_imm)};
 }
