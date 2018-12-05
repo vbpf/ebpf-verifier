363b6d7c69f4c5e706caa291630b52a2b2eb8d7e
Elazar Gershuni <elazarg@gmail.com>
2018-12-05 18:45:30 +0200

    remove redundant declaration

--- src/asm_marshal.hpp
+++ src/asm_marshal.hpp
@@ -7,8 +7,3 @@
 std::vector<ebpf_inst> marshal(Instruction ins, pc_t pc);
 std::vector<ebpf_inst> marshal(std::vector<Instruction> insts);
 // TODO marshal to ostream?
-
-Instruction parse_instruction(std::string text);
-std::vector<std::tuple<Label, Instruction>> parse_program(std::istream& is);
-
-std::ifstream open_asm_file(std::string path);
