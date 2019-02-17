e869586d87dfbda18fe67acccf52508f9ee1aa87
Elazar Gershuni <elazarg@gmail.com>
2019-02-17 12:07:51 +0200

    simpler api

--- src/main_check.cpp
+++ src/main_check.cpp
@@ -16,32 +16,25 @@
 using std::string;
 using std::vector;
 
-static int usage(const char *name)
+static int usage(string name)
 {
-    std::cerr << "usage: " << name << " [FLAGS] BINARY [TYPE] [DOMAIN]\n";
+    std::cerr << "usage: " << name << " [FLAGS] BINARY[:SECTION]\n";
     std::cerr << "\n";
-    std::cerr << "verifies the eBPF code in BINARY using DOMAIN assuming program type TYPE\n";
+    std::cerr << "check the eBPF code in BINARY (only SECTION if specified)\n";
     std::cerr << "\n";
-    std::cerr << "DOMAIN is defaulted to sdbm-arr\n";
-    std::cerr << "TYPE may be extracted from BINARY suffix\n";
+    std::cerr << "The default domain is sdbm-arr\n";
     std::cerr << "\n";
     std::cerr << "flags: "
-                 "--log=CRABLOG --verbose=N "
-                 "--stats --simplify --no-liveness --semantic-reachability\n";
+                 "--log=CRABLOG --verbose=N -v -vv --enable-warnings -l --asm "
+                 "--explicit --rcp --print-invariants --expand-locks "
+                 "--stats --no-simplify --no-liveness --semantic-reachability\n";
     std::cerr << "available domains:\n";
     for (auto const [name, desc] : domain_descriptions())
         std::cerr << "\t" << name << " - " << desc << "\n";
     return 64;
 }
 
-int main(int argc, char **argv)
-{
-    vector<string> args{argv+1, argv + argc};
-    vector<string> posargs;
-    program_info info;
-    info.program_type = BpfProgType::UNSPEC;
-    bool is_raw = true;
-    string path;
+struct cmdline_args {
     string domain = "sdbm-arr";
     string desired_section;
     string outdir = "output";
@@ -53,98 +46,106 @@ int main(int argc, char **argv)
     bool explicit_assertions = false;
     bool rcp = false;
     bool crab = false;
+    string path;
+};
+
+cmdline_args parse_args(const vector<string> args) {
+    vector<string> posargs;
+    cmdline_args res;
+    crab::CrabEnableWarningMsg(false);
+    global_options.print_invariants = false;
+    global_options.print_failures = false;
+
+    res.nondet = true;
+    res.expand_locks = true;
+    res.crab = true;
+
+    global_options.simplify = true;
+
     for (string arg : args) {
-        if (arg.find("type=") == 0) {
-            // type1 or type4
-            info.program_type = (BpfProgType)std::stoi(arg.substr(5));
-        } else if (arg.find("map") == 0) {
-            // map64 map4096 [...]
-            info.map_defs.emplace_back(map_def{.value_size=static_cast<unsigned int>(std::stoi(arg.substr(3)))});
-        } else if (arg.find("domain=") == 0) {
-            domain = arg.substr(7);
-        } else if (arg.find("elf=") == 0) {
-            arg = arg.substr(4);
-            is_raw = false;
-            if (arg.find(":") != string::npos) {
-                path = arg.substr(0, arg.find(":"));
-                desired_section = arg.substr(arg.find(":") + 1);
-            } else {
-                path = arg;
-            }
-        } else if (arg.find("raw=") == 0) {
-            is_raw = true;
-            path = arg.substr(4);
-            if (info.program_type == BpfProgType::UNSPEC) {
-                info.program_type = (BpfProgType)boost::lexical_cast<int>(path.substr(path.find_last_of('.') + 1));
-            }
+        if (arg.find("domain=") == 0) {
+            res.domain = arg.substr(7);
         } else if (arg.find("-l") == 0) {
-            list_only = true;
+            res.list_only = true;
+        } else if (arg == "--asm") {
+            res.print_asm = true;
         } else if (arg.find("--log=") == 0) {
             crab::CrabEnableLog(arg.substr(6));
-        } else if (arg == "--disable-warnings") {
-            crab::CrabEnableWarningMsg(false);
-        } else if (arg == "--asm") {
-            print_asm = true;
-        } else if (arg == "-q") {
-            crab::CrabEnableWarningMsg(false);
-            global_options.print_invariants = false;
-        } else if (arg == "-qq") {
-            crab::CrabEnableWarningMsg(false);
-            global_options.print_invariants = false;
-            global_options.print_failures = false;
+        } else if (arg == "--enable-warnings") {
+            crab::CrabEnableWarningMsg(true);
+        } else if (arg == "--print-invariants") {
+            global_options.print_invariants = true;
+        } else if (arg == "-v") {
+            crab::CrabEnableWarningMsg(true);
+            global_options.print_invariants = true;
+        } else if (arg == "-vv") {
+            crab::CrabEnableWarningMsg(true);
+            global_options.print_invariants = true;
+            global_options.print_failures = true;
         } else if (arg == "--sanity") {
             crab::CrabEnableSanityChecks(true);
         } else if (arg.find("--verbose=") == 0) {
             if (arg[0] == '"') arg=arg.substr(1, arg.size()-1);
             crab::CrabEnableVerbosity(std::stoi(arg.substr(10)));
         } else if (arg.find("--out=") == 0) {
-            outdir = arg.substr(6);
+            res.outdir = arg.substr(6);
         } else if (arg == "--help" || arg == "-h") {
-            return usage(argv[0]);
+            exit(usage(args[0]));
         } else if (arg == "--stats" || arg == "--stat") {
             global_options.stats = true;
-        } else if (arg == "--simplify") {
-            global_options.simplify = true;
+        } else if (arg == "--no-simplify") {
+            global_options.simplify = false;
         } else if (arg == "--semantic-reachability") {
             global_options.check_semantic_reachability = true;
-        } else if (arg == "--no-print-invariants") {
-            global_options.print_invariants = false;
         } else if (arg == "--no-liveness") {
             global_options.liveness = false;
         } else if (arg == "--info") {
-            info_only = true;
-        } else if (arg == "nondet") {
-            nondet = true;
-        } else if (arg == "expand_locks") {
-            expand_locks = true;
-        } else if (arg == "explicit") {
-            explicit_assertions = true;
-        } else if (arg == "rcp") {
-            nondet = true;
-            explicit_assertions = true;
-            rcp = true;
-        } else if (arg == "crab") {
-            nondet = true;
-            expand_locks = true;
-            crab = true;
+            res.info_only = true;
+        } else if (arg == "--nondet") {
+            res.nondet = true;
+        } else if (arg == "--expand_locks") {
+            res.expand_locks = true;
+        } else if (arg == "--explicit") {
+            res.explicit_assertions = true;
+        } else if (arg == "--rcp") {
+            res.nondet = true;
+            res.explicit_assertions = true;
+            res.rcp = true;
         } else {
             posargs.push_back(arg);
         }
     }
-    if (path.empty())
-        return usage(argv[0]);
+    if (posargs.size() != 1)
+        exit(usage(args[0]));
 
-    if (domain_descriptions().count(domain) == 0) {
-        std::cerr << "argument " << domain << " is not a valid domain\n";
-        return usage(argv[0]);
+    string path = posargs.back();
+    if (path.find(":") != string::npos) {
+        res.desired_section = path.substr(path.find(":") + 1);
+        res.path = path.substr(0, path.find(":"));
+    } else {
+        res.path = path;
     }
-    auto progs = is_raw ? read_raw(path, info) : read_elf(path, desired_section);
+
+    if (domain_descriptions().count(res.domain) == 0) {
+        std::cerr << "argument " << res.domain << " is not a valid domain\n";
+        exit(usage(args[0]));
+    }
+    return res;
+}
+
+int main(int argc, char **argv)
+{
+    auto args = parse_args({argv+1, argv + argc});
+    program_info info;
+    info.program_type = BpfProgType::UNSPEC;
+
+    auto progs = read_elf(args.path, args.desired_section);
     for (raw_program raw_prog : progs) {
-        if (list_only) {
+        if (args.list_only) {
             std::cout << raw_prog.filename << ":" << raw_prog.section << "\n";
             continue;
         }
-        if (info_only) {
+        if (args.info_only) {
             std::cout << "  type: " << (int)raw_prog.info.program_type;
             std::cout << "  sizes: ";
             for (auto s : raw_prog.info.map_defs) {
@@ -153,7 +154,7 @@ int main(int argc, char **argv)
             std::cout << "\n";
         } else {
             string basename = raw_prog.filename.substr(raw_prog.filename.find_last_of('/') + 1);
-            string outsubdir = outdir + "/" + basename + "/" + raw_prog.section + "/";
+            string outsubdir = args.outdir + "/" + basename + "/" + raw_prog.section + "/";
             (void)system((string() + "mkdir -p " + outsubdir).c_str());
             auto prog_or_error = unmarshal(raw_prog);
             if (std::holds_alternative<string>(prog_or_error)) {
@@ -161,19 +162,19 @@ int main(int argc, char **argv)
                 return 1;
             }
             auto& prog = std::get<InstructionSeq>(prog_or_error);
-            if (print_asm) {
+            if (args.print_asm) {
                 print(prog, std::cout);
             } else {
                 Cfg cfg = Cfg::make(prog);
-                if (explicit_assertions) {
+                if (args.explicit_assertions) {
                     explicate_assertions(cfg, raw_prog.info);
                     if (global_options.print_invariants) {
                         std::ofstream out{outsubdir + "explicit.dot"};
                         print_dot(cfg, out);
                     }
                 }
-                if (nondet) {
-                    cfg = cfg.to_nondet(expand_locks);
+                if (args.nondet) {
+                    cfg = cfg.to_nondet(args.expand_locks);
                     if (global_options.print_invariants) {
                         std::ofstream out{outsubdir + "nondet.dot"};
                         print_dot(cfg, out);
@@ -186,7 +187,7 @@ int main(int argc, char **argv)
                         print_dot(cfg, out);
                     }
                 }
-                if (rcp) {
+                if (args.rcp) {
                     analyze_rcp(cfg, raw_prog.info);
                 }
                 if (global_options.print_invariants) {
@@ -195,10 +196,10 @@ int main(int argc, char **argv)
                 }
                 if (global_options.print_invariants) {
                     std::ofstream out{outsubdir + "rcp.txt"};
-                    print(cfg, nondet,  out);
+                    print(cfg, args.nondet,  out);
                 }
-                if (crab) {
-                    const auto [res, seconds] = abs_validate(cfg, domain, raw_prog.info);
+                if (args.crab) {
+                    const auto [res, seconds] = abs_validate(cfg, args.domain, raw_prog.info);
                     std::cout << res << "," << seconds << ",";
                     std::cout << raw_prog.filename << ":" << raw_prog.section << ",";
                     std::cout << std::hex << boost::hash_range((char*)raw_prog.prog.data(), (char*)raw_prog.prog.data()+(raw_prog.prog.size() * sizeof(ebpf_inst))) << std::dec << ",";
