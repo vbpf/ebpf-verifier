8063fe592b1cf6f1dcd9f9902ea4177f3f63648a
Elazar Gershuni <elazarg@gmail.com>
2019-02-18 16:55:04 +0200

    fix verbosity and header

--- scripts/runperf.sh
+++ scripts/runperf.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-test -d $1 || (echo "first argument should be a directory"; exit 1)
+dir=$1
+test -d $dir || (echo "first argument should be a directory"; exit 1)
+shift
 
 with_timeout() {
     if hash gtimeout 2>/dev/null; then gtimeout "$@"; else timeout "$@"; fi
 }
 
-dir=$1
 files=($(find ${dir} -name '*.o'  -exec ls -Sd {} + ))
-shift
 
-if [[ "$1" == "header" ]]
-then
-	shift
-	for f in "${files[@]}"; do echo -n ",$f"; done
-fi
-
-for dom in "$@"
-do
-	echo -n ",$dom"
-done
+echo -n "file,section,hash,instructions,loads,stores,jumps,joins"
+for dom in "$@"; do echo -n ",$dom?,$dom"; done
 echo
 
 for f in "${files[@]}"
