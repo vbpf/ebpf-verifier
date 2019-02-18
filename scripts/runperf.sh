f48155e1d308e9fea17f482b18a7791fb5b2a9ee
Elazar Gershuni <elazarg@gmail.com>
2019-02-18 18:58:34 +0200

    combine precision,time,mem benchmarks

--- scripts/runperf.sh
+++ scripts/runperf.sh
@@ -8,10 +8,15 @@ with_timeout() {
     if hash gtimeout 2>/dev/null; then gtimeout "$@"; else timeout "$@"; fi
 }
 
+function readmem() { 
+	k=$(grep maximum $1 | python3 -c 'print(input().split()[0])');
+	echo $((k / 1000))
+}
+
 files=($(find ${dir} -name '*.o'  -exec ls -Sd {} + ))
 
-echo -n "file,section,hash,instructions,loads,stores,jumps,joins"
-for dom in "$@"; do echo -n ",$dom?,$dom"; done
+echo -n "suite,project,file,section,hash,instructions,loads,stores,jumps,joins"
+for dom in "$@"; do echo -n ",$dom?,$dom-sec,$dom-kb"; done
 echo
 
 for f in "${files[@]}"
@@ -19,11 +24,17 @@ do
 	sections=($(./check $f -l))
 	for s in "${sections[@]}"
 	do
+		echo -n $f | tr / ,
+		echo -n ,$s,
 		echo -n $(./check $f $s --domain=stats)
 		for dom in "$@"
 		do
-			k=$(with_timeout 10m ./check $f $s --domain=$dom)
-			echo -n ",$k"
+			tmp=$(mktemp /tmp/mem.XXXXXX)
+
+			k=$(with_timeout 10m /usr/bin/time -l ./check $f $s --domain=$dom 2>$tmp)
+			m=$(readmem $tmp); rm "$tmp"
+			echo -n ",$k,$m"
+
 		done
 		echo
 	done
