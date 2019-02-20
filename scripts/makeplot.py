b1a85404ba353bdf3a5b08233a486ca00d1dbac7
Elazar Gershuni <elazarg@gmail.com>
2019-02-20 14:01:46 +0200

    Update makeplot.py

--- scripts/makeplot.py
+++ scripts/makeplot.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -8,7 +7,7 @@ data = np.genfromtxt(sys.argv[1], delimiter=',', names=True)
 n = 0
 fig = plt.figure()
 
-def plot(title, field, units, values):
+def plot(title, field, units, suffix):
     global n
     n += 1
     sp = fig.add_subplot(1, 2, n)
@@ -17,32 +16,21 @@ def plot(title, field, units, values):
     sp.set_ylabel(units)
     field = data[field]
     space = np.linspace(min(field), max(field), 2)
-
-    for (label, deg, color) in values:
+    for label in data.dtype.fields:
+        if not label.endswith(suffix): continue
+        deg = 1
         arr = data[label]
         trendline = np.poly1d(np.polyfit(field, arr, deg))(space)
+        color = next(plt.gca()._get_lines.prop_cycler)['color']
         sp.plot(space, trendline, color=color)
         sp.plot(field, arr, color=color, label=label,
                         marker='.',
                         markerfacecolor='None',
-                        markeredgecolor=color,
                         linestyle = 'None')
     sp.legend()
 
-plot("Sec vs stores", 'stores', 'Time (Sec)', [
-    ('interval_sec' , 1, 'blue'),
-    ('zoneCrab_sec' , 1, 'red'),
-    ('zoneElina_sec', 1, 'orange'),
-    ('polyElina_sec', 1, 'green'),
-    #('octElina_sec' , 1, 'purple'),
-])
+plot("Sec vs stores", 'stores', 'Time (Sec)', '_sec')
 
-plot("Memory vs stores", 'stores', 'Memory (KB)', [
-    ('interval_kb' , 1, 'blue'),
-    ('zoneCrab_kb' , 1, 'red'),
-    ('zoneElina_kb', 1, 'orange'),
-    ('polyElina_kb', 1, 'green'),
-    #('octElina_kb' , 1, 'purple'),
-])
+plot("Memory vs stores", 'stores', 'Memory (KB)', '_kb')
 
 plt.show()
