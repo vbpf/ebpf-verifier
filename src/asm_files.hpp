63d4ad7910b24b15d4de0571da343160b86da3ae
Elazar Gershuni <elazarg@gmail.com>
2019-03-03 14:55:34 +0200

    create_map

--- src/asm_files.hpp
+++ src/asm_files.hpp
@@ -7,6 +7,8 @@
 #include "asm_syntax.hpp"
 #include "spec_type_descriptors.hpp"
 
+using MapFd = auto (uint32_t map_type, uint32_t key_size, uint32_t value_size, uint32_t max_entries) -> int;
+
 std::vector<raw_program> read_raw(std::string path, program_info info);
 std::vector<raw_program> read_elf(std::string path, std::string section);
 
