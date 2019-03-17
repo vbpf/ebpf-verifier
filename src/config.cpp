7223629d1da78d0d7617acf414f73cfacda78a3e
Elazar Gershuni <elazarg@gmail.com>
2019-03-17 20:57:12 +0200

    saner assumptions, encode mapdef in fd

--- src/config.cpp
+++ src/config.cpp
@@ -1,7 +1,7 @@
 #include "config.hpp"
 
 global_options_t global_options {
-    .simplify = true,
+    .simplify = false,
     .check_semantic_reachability = false,
     .print_invariants = false,
     .print_failures = false,
