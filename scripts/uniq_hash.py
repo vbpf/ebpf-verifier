6fc458e2077a86d168253226378b9bff7de122ab
Elazar Gershuni <elazarg@gmail.com>
2019-03-25 22:00:15 +0200

    uniq_hash.py

---
+++ scripts/uniq_hash.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import fileinput
+import sys
+
+f = fileinput.input()
+header = next(f)
+sys.stdout.write(header)
+h_index = header.split(',').index('hash')
+
+seen = set()
+for line in f:
+    h = line.split(',')[h_index]
+    if h in seen: continue
+    seen.add(h)
+    sys.stdout.write(line)
