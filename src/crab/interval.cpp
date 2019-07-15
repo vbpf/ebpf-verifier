4dc7466459fca1b5e55ea5ff0ac07b827320caf3
Elazar Gershuni <elazarg@gmail.com>
2019-07-15 11:39:03 +0300

    interval.cpp

---
+++ src/crab/interval.cpp
@@ -0,0 +1,248 @@
+#include "crab/interval.hpp"
+
+namespace ikos {
+
+interval_t interval_t::operator/(interval_t x) const {
+    if (is_bottom() || x.is_bottom()) {
+        return bottom();
+    } else {
+        // Divisor is a singleton:
+        //   the linear interval solver can perform many divisions where
+        //   the divisor is a singleton interval. We optimize for this case.
+        if (std::optional<number_t> n = x.singleton()) {
+            number_t c = *n;
+            if (c == 1) {
+                return *this;
+            } else if (c > 0) {
+                return interval_t(_lb / (bound_t)c, _ub / (bound_t)c);
+            } else if (c < 0) {
+                return interval_t(_ub / (bound_t)c, _lb / (bound_t)c);
+            } else {
+            }
+        }
+        // Divisor is not a singleton
+        using z_interval = interval_t;
+        if (x[0]) {
+            z_interval l(x._lb, z_bound(-1));
+            z_interval u(z_bound(1), x._ub);
+            return (operator/(l) | operator/(u));
+        } else if (operator[](0)) {
+            z_interval l(_lb, z_bound(-1));
+            z_interval u(z_bound(1), _ub);
+            return ((l / x) | (u / x) | z_interval(z_number(0)));
+        } else {
+            // Neither the dividend nor the divisor contains 0
+            z_interval a = (_ub < 0)
+                               ? (*this + ((x._ub < 0) ? (x + z_interval(z_number(1))) : (z_interval(z_number(1)) - x)))
+                               : *this;
+            bound_t ll = a._lb / x._lb;
+            bound_t lu = a._lb / x._ub;
+            bound_t ul = a._ub / x._lb;
+            bound_t uu = a._ub / x._ub;
+            return interval_t(bound_t::min(ll, lu, ul, uu), bound_t::max(ll, lu, ul, uu));
+        }
+    }
+}
+
+interval_t interval_t::SRem(interval_t x) const {
+    // note that the sign of the divisor does not matter
+
+    if (is_bottom() || x.is_bottom()) {
+        return bottom();
+    } else if (singleton() && x.singleton()) {
+        z_number dividend = *singleton();
+        z_number divisor = *x.singleton();
+
+        if (divisor == 0) {
+            return bottom();
+        }
+
+        return interval_t(dividend % divisor);
+    } else if (x.ub().is_finite() && x.lb().is_finite()) {
+        z_number max_divisor = max(abs(*x.lb().number()), abs(*x.ub().number()));
+
+        if (max_divisor == 0) {
+            return bottom();
+        }
+
+        if (lb() < 0) {
+            if (ub() > 0) {
+                return interval_t(-(max_divisor - 1), max_divisor - 1);
+            } else {
+                return interval_t(-(max_divisor - 1), 0);
+            }
+        } else {
+            return interval_t(0, max_divisor - 1);
+        }
+    } else {
+        return top();
+    }
+}
+
+interval_t interval_t::URem(interval_t x) const {
+
+    if (is_bottom() || x.is_bottom()) {
+        return bottom();
+    } else if (singleton() && x.singleton()) {
+        z_number dividend = *singleton();
+        z_number divisor = *x.singleton();
+
+        if (divisor < 0) {
+            return top();
+        } else if (divisor == 0) {
+            return bottom();
+        } else if (dividend < 0) {
+            // dividend is treated as an unsigned integer.
+            // we would need the size to be more precise
+            return interval_t(0, divisor - 1);
+        } else {
+            return interval_t(dividend % divisor);
+        }
+    } else if (x.ub().is_finite() && x.lb().is_finite()) {
+        z_number max_divisor = *x.ub().number();
+
+        if (x.lb() < 0 || x.ub() < 0) {
+            return top();
+        } else if (max_divisor == 0) {
+            return bottom();
+        }
+
+        return interval_t(0, max_divisor - 1);
+    } else {
+        return top();
+    }
+}
+
+interval_t interval_t::And(interval_t x) const {
+    if (is_bottom() || x.is_bottom()) {
+        return bottom();
+    } else {
+        std::optional<z_number> left_op = singleton();
+        std::optional<z_number> right_op = x.singleton();
+
+        if (left_op && right_op) {
+            return interval_t((*left_op) & (*right_op));
+        } else if (lb() >= 0 && x.lb() >= 0) {
+            return interval_t(0, bound_t::min(ub(), x.ub()));
+        } else {
+            return top();
+        }
+    }
+}
+
+interval_t interval_t::Or(interval_t x) const {
+    if (is_bottom() || x.is_bottom()) {
+        return bottom();
+    } else {
+        std::optional<z_number> left_op = singleton();
+        std::optional<z_number> right_op = x.singleton();
+
+        if (left_op && right_op) {
+            return interval_t((*left_op) | (*right_op));
+        } else if (lb() >= 0 && x.lb() >= 0) {
+            std::optional<z_number> left_ub = ub().number();
+            std::optional<z_number> right_ub = x.ub().number();
+
+            if (left_ub && right_ub) {
+                z_number m = (*left_ub > *right_ub ? *left_ub : *right_ub);
+                return interval_t(0, m.fill_ones());
+            } else {
+                return interval_t(0, bound_t::plus_infinity());
+            }
+        } else {
+            return top();
+        }
+    }
+}
+
+interval_t interval_t::Xor(interval_t x) const {
+    if (is_bottom() || x.is_bottom()) {
+        return bottom();
+    } else {
+        std::optional<z_number> left_op = singleton();
+        std::optional<z_number> right_op = x.singleton();
+
+        if (left_op && right_op) {
+            return interval_t((*left_op) ^ (*right_op));
+        } else {
+            return Or(x);
+        }
+    }
+}
+
+interval_t interval_t::Shl(interval_t x) const {
+    if (is_bottom() || x.is_bottom()) {
+        return bottom();
+    } else {
+        if (std::optional<z_number> shift = x.singleton()) {
+            z_number k = *shift;
+            if (k < 0) {
+                // CRAB_ERROR("lshr shift operand cannot be negative");
+                return top();
+            }
+            // Some crazy linux drivers generate shl instructions with
+            // huge shifts.  We limit the number of times the loop is run
+            // to avoid wasting too much time on it.
+            if (k <= 128) {
+                z_number factor = 1;
+                for (int i = 0; k > i; i++) {
+                    factor *= 2;
+                }
+                return (*this) * factor;
+            }
+        }
+        return top();
+    }
+}
+
+interval_t interval_t::AShr(interval_t x) const {
+    if (is_bottom() || x.is_bottom()) {
+        return bottom();
+    } else {
+        if (std::optional<z_number> shift = x.singleton()) {
+            z_number k = *shift;
+            if (k < 0) {
+                // CRAB_ERROR("ashr shift operand cannot be negative");
+                return top();
+            }
+            // Some crazy linux drivers generate ashr instructions with
+            // huge shifts.  We limit the number of times the loop is run
+            // to avoid wasting too much time on it.
+            if (k <= 128) {
+                z_number factor = 1;
+                for (int i = 0; k > i; i++) {
+                    factor *= 2;
+                }
+                return (*this) / factor;
+            }
+        }
+        return top();
+    }
+}
+
+interval_t interval_t::LShr(interval_t x) const {
+    if (is_bottom() || x.is_bottom()) {
+        return bottom();
+    } else {
+        if (std::optional<z_number> shift = x.singleton()) {
+            z_number k = *shift;
+            if (k < 0) {
+                // CRAB_ERROR("lshr shift operand cannot be negative");
+                return top();
+            }
+            // Some crazy linux drivers generate lshr instructions with
+            // huge shifts.  We limit the number of times the loop is run
+            // to avoid wasting too much time on it.
+            if (k <= 128) {
+                if (lb() >= 0 && ub().is_finite() && shift) {
+                    z_number lb = *this->lb().number();
+                    z_number ub = *this->ub().number();
+                    return interval_t(lb >> k, ub >> k);
+                }
+            }
+        }
+        return this->top();
+    }
+}
+
+} // namespace ikos
