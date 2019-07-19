3e7d7cf90ee4f5902de76dcdd76201aecdec1eda
Elazar Gershuni <elazarg@gmail.com>
2019-07-19 17:07:56 +0300

    move prints to cfg.cpp

--- src/crab/cfg.cpp
+++ src/crab/cfg.cpp
@@ -70,10 +70,49 @@ class variable_ref_t {
     void write(crab_os& o) const { return m_v->write(o); }
 }; // class variable_ref_t
 
-inline crab_os& operator<<(crab_os& o, const variable_ref_t& v) {
+crab_os& operator<<(crab_os& o, const variable_ref_t& v) {
     v.write(o);
     return o;
 }
+
+static crab_os& operator<<(crab_os& o, const binary_op_t& s) {
+    return o << s.lhs << " = " << s.left << s.op << s.right;
+}
+static crab_os& operator<<(crab_os& o, const assign_t& s) { return o << s.lhs << " = " << s.rhs; }
+static crab_os& operator<<(crab_os& o, const assume_t& s) { return o << "assume(" << s.constraint << ")"; }
+static crab_os& operator<<(crab_os& o, const havoc_t& s) { return o << "havoc(" << s.lhs << ")"; }
+static crab_os& operator<<(crab_os& o, const select_t& s) {
+    return o << s.lhs << " = "
+             << "ite(" << s.cond << "," << s.left << "," << s.right << ")";
+}
+static crab_os& operator<<(crab_os& o, const assert_t& s) {
+    o << "assert(" << s.constraint << ")";
+    if (s.debug.has_debug()) {
+        o << " // line=" << s.debug.m_line << " column=" << s.debug.m_col;
+    }
+    return o;
+}
+static crab_os& operator<<(crab_os& o, const array_init_t& s) {
+    return o << s.array << "[" << s.lb_index << "..." << s.ub_index << "] := " << s.val;
+}
+static crab_os& operator<<(crab_os& o, const array_store_t& s) {
+    o << "array_store(" << s.array << "," << s.lb_index;
+    if (!s.lb_index.equal(s.ub_index)) {
+        o << ".." << s.ub_index;
+    }
+    o << "," << s.value << ",sz=" << s.elem_size << ")";
+    return o;
+}
+static crab_os& operator<<(crab_os& o, const array_load_t& s) {
+    return o << s.lhs << " = "
+             << "array_load(" << s.array << "," << s.index << ",sz=" << s.elem_size << ")";
+}
+
+crab_os& operator<<(crab_os& os, const new_statement_t& a) {
+    std::visit([&](const auto& arg) { os << arg; }, a);
+    return os;
+}
+
 
 void cfg_t::remove_useless_blocks() {
     if (!has_exit())
