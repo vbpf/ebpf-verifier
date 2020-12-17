a3c18684d82d0b4e7e9de84fddb40e77acb88738
Elazar Gershuni <elazarg@gmail.com>
2020-12-17 17:46:30 +0200

    fix loop tests
    Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

--- src/test/test_loop.cpp
+++ src/test/test_loop.cpp
@@ -10,28 +10,12 @@ using namespace crab;
 
 constexpr Bin NOP = Bin{.op = Bin::Op::MOV, .dst = Reg{1}, .v = Reg{1}};
 
-TEST_CASE("Trivial loop: entry <-> exit", "[sanity][loop]") {
-    cfg_t cfg(label_t(0), label_t(2));
-
-    basic_block_t& entry = cfg.get_node(cfg.entry());
-    basic_block_t& exit = cfg.get_node(cfg.exit());
-
-    entry.insert(Bin{.op = Bin::Op::MOV, .dst = Reg{0}, .v = Imm{0}, .is64 = true});
-    exit.insert(Bin{.op = Bin::Op::ADD, .dst = Reg{0}, .v = Imm{1}, .is64 = true});
-
-    entry >> exit;
-    exit >> exit;
-
-    auto [pass, time] = run_ebpf_analysis(cfg, {});
-    REQUIRE(pass);
-}
-
 TEST_CASE("Trivial loop: middle", "[sanity][loop]") {
-    cfg_t cfg(label_t(0), label_t(2));
+    cfg_t cfg;
 
-    basic_block_t& entry = cfg.get_node(cfg.entry());
+    basic_block_t& entry = cfg.get_node(cfg.entry_label());
     basic_block_t& middle = cfg.insert(label_t(1));
-    basic_block_t& exit = cfg.get_node(cfg.exit());
+    basic_block_t& exit = cfg.get_node(cfg.exit_label());
 
     entry.insert(Bin{.op = Bin::Op::MOV, .dst = Reg{0}, .v = Imm{0}, .is64 = true});
     middle.insert(Bin{.op = Bin::Op::ADD, .dst = Reg{0}, .v = Imm{1}, .is64 = true});
@@ -39,24 +23,6 @@ TEST_CASE("Trivial loop: middle", "[sanity][loop]") {
     entry >> middle;
     middle >> middle;
     middle >> exit;
-
-    auto [pass, time] = run_ebpf_analysis(cfg, {});
-    REQUIRE(pass);
-}
-
-TEST_CASE("Trivial loop: entry -> middle <-> exit", "[sanity][loop]") {
-    cfg_t cfg(label_t(0), label_t(2));
-
-    basic_block_t& entry = cfg.get_node(cfg.entry());
-    basic_block_t& middle = cfg.insert(label_t(1));
-    basic_block_t& exit = cfg.get_node(cfg.exit());
-
-    entry.insert(Bin{.op = Bin::Op::MOV, .dst = Reg{0}, .v = Imm{0}, .is64 = true});
-    middle.insert(Bin{.op = Bin::Op::ADD, .dst = Reg{0}, .v = Imm{1}, .is64 = true});
-
-    entry >> middle;
-    middle >> exit;
-    exit >> middle;
 
     auto [pass, time] = run_ebpf_analysis(cfg, {});
     REQUIRE(pass);
