306d0e9668c8a27c63eb7224609f09b22c0bf2d3
Elazar Gershuni <elazarg@gmail.com>
2021-01-05 16:30:39 +0200

    Do not try to add instruction to the "dummy" entry.
    Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

--- src/test/test_loop.cpp
+++ src/test/test_loop.cpp
@@ -4,22 +4,20 @@
 
 #include "crab/cfg.hpp"
 #include "crab_verifier.hpp"
-#include "config.hpp"
 
 using namespace crab;
-
-constexpr Bin NOP = Bin{.op = Bin::Op::MOV, .dst = Reg{1}, .v = Reg{1}};
 
 TEST_CASE("Trivial loop: middle", "[sanity][loop]") {
     cfg_t cfg;
 
-    basic_block_t& entry = cfg.get_node(cfg.entry_label());
+    basic_block_t& entry = cfg.insert(label_t(0));
     basic_block_t& middle = cfg.insert(label_t(1));
     basic_block_t& exit = cfg.get_node(cfg.exit_label());
 
     entry.insert(Bin{.op = Bin::Op::MOV, .dst = Reg{0}, .v = Imm{0}, .is64 = true});
     middle.insert(Bin{.op = Bin::Op::ADD, .dst = Reg{0}, .v = Imm{1}, .is64 = true});
 
+    cfg.get_node(cfg.entry_label()) >> entry;
     entry >> middle;
     middle >> middle;
     middle >> exit;
