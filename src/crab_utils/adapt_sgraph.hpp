58b33c16aef58aa81b477685e93c860371120de3
Elazar Gershuni <elazarg@gmail.com>
2021-09-01 20:26:53 +0300

    Use boost::container::flat_map in TreeSMap
    The command
    ""./check ebpf-samples/bpf_cilium_test/bpf_lxc_jit.o 2/10"
    seems to run ~20% faster.
    
    Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

--- src/crab_utils/adapt_sgraph.hpp
+++ src/crab_utils/adapt_sgraph.hpp
@@ -4,6 +4,8 @@
 
 #include <memory>
 
+#include <boost/container/flat_map.hpp>
+
 #include "crab_utils/safeint.hpp"
 #include "crab_utils/debug.hpp"
 // Adaptive sparse-set based weighted graph implementation
@@ -16,7 +18,7 @@ class TreeSMap final {
     using val_t = size_t;
 
   private:
-    using col = std::map<key_t, val_t>;
+    using col = boost::container::flat_map<key_t, val_t>;
     col map;
 
   public:
@@ -160,7 +162,7 @@ class AdaptGraph final {
 
     struct edge_iter {
         struct edge_ref {
-            vert_id vert;
+            vert_id vert{};
             Wt val;
         };
 
