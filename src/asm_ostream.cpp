vbpf/ebpf-verifier

commit 0f8b3de6e7a199f955cf88937da5a4fa7a5a71a9

more consistent output for cfg and invariants
Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

src/asm_ostream.cpp
@@ -21,7 +21,7 @@ std::ostream& operator<<(std::ostream& os, ArgSingle::Kind kind) {
     case ArgSingle::Kind::ANYTHING: return os << "uint64_t";
     case ArgSingle::Kind::PTR_TO_CTX: return os << "ctx";
     case ArgSingle::Kind::MAP_FD: return os << "map_fd";
-    case ArgSingle::Kind::MAP_FD_PROGRAMS: return os << "map_fd (programs)";
+    case ArgSingle::Kind::MAP_FD_PROGRAMS: return os << "map_fd_programs";
     case ArgSingle::Kind::PTR_TO_MAP_KEY: return os << "map_key";
     case ArgSingle::Kind::PTR_TO_MAP_VALUE: return os << "map_value";
     }
@@ -94,43 +94,60 @@ std::ostream& operator<<(std::ostream& os, Condition::Op op) {
 
 static string size(int w) { return string("u") + std::to_string(w * 8); }
 
-std::ostream& operator<<(std::ostream& os, TypeGroup ts) {
+static std::string to_string(TypeGroup ts) {
     switch (ts) {
-    case TypeGroup::number: return os << "number";
-    case TypeGroup::map_fd: return os << "map_fd";
-    case TypeGroup::map_fd_programs: return os << "map_fd_programs";
-    case TypeGroup::ctx: return os << "ctx";
-    case TypeGroup::packet: return os << "packet";
-    case TypeGroup::stack: return os << "stack";
-    case TypeGroup::shared: return os << "shared";
-    case TypeGroup::mem: return os << "mem";
-    case TypeGroup::pointer: return os << "pointer";
-    case TypeGroup::non_map_fd: return os << "non_map_fd";
-    case TypeGroup::ptr_or_num: return os << "pointer_or_number";
-    case TypeGroup::stack_or_packet: return os << "stack_or_packet";
-    case TypeGroup::mem_or_num: return os << "mem_or_number";
+    case TypeGroup::number: return "number";
+    case TypeGroup::map_fd: return "map_fd";
+    case TypeGroup::map_fd_programs: return "map_fd_programs";
+    case TypeGroup::ctx: return "ctx";
+    case TypeGroup::packet: return "packet";
+    case TypeGroup::stack: return "stack";
+    case TypeGroup::shared: return "shared";
+    case TypeGroup::mem: return "{stack, packet, shared}";
+    case TypeGroup::pointer: return "{ctx, stack, packet, shared}";
+    case TypeGroup::non_map_fd: return "non_map_fd";
+    case TypeGroup::ptr_or_num: return "{number, ctx, stack, packet, shared}";
+    case TypeGroup::stack_or_packet: return "{stack, packet}";
+    case TypeGroup::mem_or_num: return "{number, stack, packet, shared}";
+    default: assert(false);
     }
-    return os;
+    return {};
+}
+
+std::ostream& operator<<(std::ostream& os, TypeGroup ts) {
+    return os << to_string(ts);
 }
 
 std::ostream& operator<<(std::ostream& os, ValidStore const& a) {
-    return os << "!stack(" << a.mem << ") -> num(" << a.val << ")";
+    return os << a.mem << ".type != stack -> " << a.val << ".type == num";
 }
 
 std::ostream& operator<<(std::ostream& os, ValidAccess const& a) {
     if (a.or_null)
-        os << a.reg << " == 0 or ";
-    return os << "valid_access(" << a.reg << ", " << a.offset << ":" << a.width << ")";
+        os << a.reg << ".type == number and " << a.reg << ".value == 0";
+    os << "valid_access(" << a.reg << ".offset";
+    if (a.offset > 0)
+        os << "+" << a.offset;
+    else if (a.offset < 0)
+        os << a.offset;
+
+    if (a.width == (Value)Imm{0}) {
+        // a.width == 0, meaning we only care it's an in-bound pointer,
+        // so it can be compared with another pointer to the same region.
+        os << ") for comparison";
+    } else {
+        os << ", width=" << a.width << ")";
+    }
+    return os;
 }
 
 std::ostream& operator<<(std::ostream& os, ValidSize const& a) {
     auto op = a.can_be_zero ? " >= " : " > ";
-    return os << a.reg << op << 0;
+    return os << a.reg << ".value" << op << 0;
 }
 
 std::ostream& operator<<(std::ostream& os, ValidMapKeyValue const& a) {
-    return os << "within stack(" << a.access_reg << ":" << (a.key ? "key_size" : "value_size") << "(" << a.map_fd_reg
-              << "))";
+    return os << "within stack(" << a.access_reg << ":" << (a.key ? "key_size" : "value_size") << "(" << a.map_fd_reg << "))";
 }
 
 std::ostream& operator<<(std::ostream& os, ZeroOffset const& a) {
@@ -143,10 +160,14 @@ std::ostream& operator<<(std::ostream& os, Comparable const& a) {
 }
 
 std::ostream& operator<<(std::ostream& os, Addable const& a) {
-    return os << a.ptr << " : ptr -> " << a.num << " : num";
+    return os << a.ptr << ".type = ptr -> " << a.num << ".type = number";
 }
 
-std::ostream& operator<<(std::ostream& os, TypeConstraint const& tc) { return os << tc.reg << " is " << tc.types; }
+std::ostream& operator<<(std::ostream& os, TypeConstraint const& tc) {
+    string types = to_string(tc.types);
+    string cmp_op = types[0] == '{' ? "in" : "==";
+    return os << tc.reg << ".type " << cmp_op << " " << tc.types;
+}
 
 std::ostream& operator<<(std::ostream& os, AssertionConstraint const& a) {
     return std::visit([&](const auto& a) -> std::ostream& { return os << a; }, a);

src/crab_verifier.cpp
@@ -60,19 +60,19 @@ struct checks_db final {
 
 static checks_db generate_report(std::ostream& s,
                                  cfg_t& cfg,
-                                 crab::invariant_table_t& preconditions,
-                                 crab::invariant_table_t& postconditions) {
+                                 crab::invariant_table_t& pre_invariants,
+                                 crab::invariant_table_t& post_invariants) {
     checks_db m_db;
     for (const label_t& label : cfg.sorted_labels()) {
         basic_block_t& bb = cfg.get_node(label);
 
         if (thread_local_options.print_invariants) {
-            s << "\nPreconditions : " << preconditions.at(label) << "\n";
+            s << "\nPre-invariant : " << pre_invariants.at(label) << "\n";
             s << bb;
-            s << "\nPostconditions: " << postconditions.at(label) << "\n";
+            s << "\nPost-invariant: " << post_invariants.at(label) << "\n";
         }
 
-        ebpf_domain_t from_inv(preconditions.at(label));
+        ebpf_domain_t from_inv(pre_invariants.at(label));
         from_inv.set_require_check([&m_db, label](auto& inv, const linear_constraint_t& cst, const std::string& s) {
             if (inv.is_bottom())
                 return;
@@ -95,7 +95,7 @@ static checks_db generate_report(std::ostream& s,
             // Pinpoint the places where divergence might occur.
             int min_instruction_count_upper_bound = INT_MAX;
             for (const label_t& prev_label : bb.prev_blocks_set()) {
-                int instruction_count = preconditions.at(prev_label).get_instruction_count_upper_bound();
+                int instruction_count = pre_invariants.at(prev_label).get_instruction_count_upper_bound();
                 min_instruction_count_upper_bound = std::min(min_instruction_count_upper_bound, instruction_count);
             }
 
@@ -153,12 +153,12 @@ static checks_db get_ebpf_report(std::ostream& s, cfg_t& cfg, program_info info,
     thread_local_options = *options;
 
     try {
-        // Get dictionaries of preconditions and postconditions for each
+        // Get dictionaries of pre-invariants and post-invariants for each
         // basic block.
-        auto [preconditions, postconditions] = crab::run_forward_analyzer(cfg, options->check_termination);
+        auto [pre_invariants, post_invariants] = crab::run_forward_analyzer(cfg, options->check_termination);
 
         // Analyze the control-flow graph.
-        return generate_report(s, cfg, preconditions, postconditions);
+        return generate_report(s, cfg, pre_invariants, post_invariants);
     } catch (std::runtime_error& e) {
         // Convert verifier runtime_error exceptions to failure.
         checks_db db;

commit 33d571ae16938d86348559bb37f499e8caf5fef3

Fix output regression from PR #257
An "or" was output before PR #257 but it got lost in that PR, along
with the space before "valid_access", resulting in output like:

"Lower bound must be at least 0 (r3.type == number and r3.value ==
0valid_access(r3.offset, width=r4))"

Signed-off-by: Dave Thaler <dthaler@microsoft.com>

src/asm_ostream.cpp
@@ -124,7 +124,7 @@ std::ostream& operator<<(std::ostream& os, ValidStore const& a) {
 
 std::ostream& operator<<(std::ostream& os, ValidAccess const& a) {
     if (a.or_null)
-        os << a.reg << ".type == number and " << a.reg << ".value == 0";
+        os << "(" << a.reg << ".type == number and " << a.reg << ".value == 0) or ";
     os << "valid_access(" << a.reg << ".offset";
     if (a.offset > 0)
         os << "+" << a.offset;