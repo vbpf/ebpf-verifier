8bc147e42bf74f9c0eb7d32720e7bc3da99f8a50
Alan Jowett <alanjo@microsoft.com>
2022-02-17 13:12:49 -0700

    PR feedback
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- src/btf_parser.h
+++ src/btf_parser.h
@@ -11,11 +11,11 @@ typedef std::function<void(const std::string& section, uint32_t instruction_offs
                            const std::string& source, uint32_t line_number, uint32_t column_number)>
     btf_line_info_visitor;
 /**
- * @brief Parse a .btf and .btf.ext section from an ELF file invoke vistor for
+ * @brief Parse a .BTF and .BTF.ext section from an ELF file invoke vistor for
  * each btf_line_info record.
  *
- * @param[in] btf The .btf section (containing type info and strings).
- * @param[in] btf_ext The .btf.ext section (containing function info and
+ * @param[in] btf The .BTF section (containing type info and strings).
+ * @param[in] btf_ext The .BTF.ext section (containing function info and
  * line info).
  * @param[in] visitor Function to invoke on each btf_line_info record.
  */
