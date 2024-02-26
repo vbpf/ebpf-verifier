cccf82faaf4abffaf2e2d350370042eb04da193a
Dave Thaler <dthaler1968@gmail.com>
2024-02-26 13:01:27 -0800

    Update per bpf_conformance API change
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- src/test/test_conformance.cpp
+++ src/test/test_conformance.cpp
@@ -12,9 +12,9 @@ void test_conformance(std::string filename, bpf_conformance_test_result_t expect
     boost::filesystem::path extension = test_path.extension();
     std::filesystem::path plugin_path =
         test_path.remove_filename().append("conformance_check" + extension.string()).string();
-    std::map<std::filesystem::path, std::tuple<bpf_conformance_test_result_t, std::string>> result =
-        bpf_conformance(test_files, plugin_path, {}, {}, {}, bpf_conformance_test_CPU_version_t::v4,
-                        bpf_conformance_list_instructions_t::LIST_INSTRUCTIONS_NONE, true);
+    std::map<std::filesystem::path, std::tuple<bpf_conformance_test_result_t, std::string>> result = bpf_conformance(
+        test_files, plugin_path, {}, {}, {}, bpf_conformance_test_CPU_version_t::v4,
+        _bpf_conformance_groups::default_groups, bpf_conformance_list_instructions_t::LIST_INSTRUCTIONS_NONE, true);
     for (auto file : test_files) {
         auto& [file_result, reason] = result[file];
         REQUIRE(file_result == expected_result);
