12d3121891d6ef434cc723d3595676626cf827d2
Elazar Gershuni <elazarg@gmail.com>
2024-09-29 05:41:05 +0300

    modernize heap.hpp (#700)
    modernize Heap, remove unused .clear() method
    
    Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

--- src/crab_utils/heap.hpp
+++ src/crab_utils/heap.hpp
@@ -23,44 +23,44 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ******************************************************************************************/
 #include <vector>
 
-//=========================================================================================
-// A heap implementation with support for decrease/increase key.
-//=========================================================================================
-
 namespace crab {
 
-template <class Comp>
+// A heap implementation with support for decrease/increase key.
+// @tparam Comp a predicate that compares two integers.
+template <std::predicate<int, int> Comparator>
 class Heap {
-    Comp lt;
+    Comparator lt;
     std::vector<int> heap;    // heap of ints
     std::vector<int> indices; // int -> index in heap
 
     // Index "traversal" functions
-    static inline int left(int i) { return i * 2 + 1; }
-    static inline int right(int i) { return (i + 1) * 2; }
-    static inline int parent(int i) { return (i - 1) >> 1; }
+    static int left(const int i) { return i * 2 + 1; }
+    static int right(const int i) { return (i + 1) * 2; }
+    static int parent(const int i) { return (i - 1) >> 1; }
 
-    inline void percolateUp(int i) {
+    void percolateUp(int i) {
         int x = heap[i];
         while (i != 0 && lt(x, heap[parent(i)])) {
-            heap[i] = heap[parent(i)];
-            indices[heap[i]] = i;
+            const int v = heap[i] = heap[parent(i)];
+            indices[v] = i;
             i = parent(i);
         }
         heap[i] = x;
         indices[x] = i;
     }
 
-    inline void percolateDown(int i) {
-        int x = heap[i];
-        while (static_cast<size_t>(left(i)) < heap.size()) {
-            int child =
-                static_cast<size_t>(right(i)) < heap.size() && lt(heap[right(i)], heap[left(i)]) ? right(i) : left(i);
+    void percolateDown(int i) {
+        const int x = heap[i];
+        const int size = heap.size();
+        while (left(i) < size) {
+            int ri = right(i);
+            int li = left(i);
+            const int child = static_cast<size_t>(ri) < size && lt(heap[ri], heap[li]) ? ri : li;
             if (!lt(heap[child], x)) {
                 break;
             }
-            heap[i] = heap[child];
-            indices[heap[i]] = i;
+            const int v = heap[i] = heap[child];
+            indices[v] = i;
             i = child;
         }
         heap[i] = x;
@@ -68,13 +68,13 @@ class Heap {
     }
 
     [[nodiscard]]
-    bool heapProperty(int i) const {
+    bool heapProperty(const int i) const {
         return i >= heap.size() ||
                ((i == 0 || !lt(heap[i], heap[parent(i)])) && heapProperty(left(i)) && heapProperty(right(i)));
     }
 
   public:
-    explicit Heap(const Comp& c) : lt(c) {}
+    explicit Heap(const Comparator& c) : lt(c) {}
 
     [[nodiscard]]
     int size() const {
@@ -85,20 +85,20 @@ class Heap {
         return heap.empty();
     }
     [[nodiscard]]
-    bool inHeap(int n) const {
+    bool inHeap(const int n) const {
         return static_cast<size_t>(n) < indices.size() && indices[n] >= 0;
     }
-    int operator[](int index) const {
+    int operator[](const int index) const {
         assert(static_cast<size_t>(index) < heap.size());
         return heap[index];
     }
 
-    void decrease(int n) {
+    void decrease(const int n) {
         assert(inHeap(n));
         percolateUp(indices[n]);
     }
 
-    void insert(int n) {
+    void insert(const int n) {
         assert(n >= 0);
         if (static_cast<size_t>(n) >= indices.size()) {
             indices.resize(n + 1, -1);
@@ -111,27 +111,15 @@ class Heap {
     }
 
     int removeMin() {
-        int x = heap[0];
-        heap[0] = heap.back();
-        indices[heap[0]] = 0;
+        const int x = heap[0];
+        const int v = heap[0] = heap.back();
+        indices[v] = 0;
         indices[x] = -1;
         heap.pop_back();
         if (heap.size() > 1) {
             percolateDown(0);
         }
         return x;
-    }
-
-    void clear() {
-        for (int i : heap) {
-            indices[i] = -1;
-        }
-#ifdef NDEBUG
-        for (int i = 0; i < indices.size(); i++) {
-            assert(indices[i] == -1);
-        }
-#endif
-        heap.clear();
     }
 };
 } // namespace crab
