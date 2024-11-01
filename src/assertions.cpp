82f1e4039aaa0b96a4a9b26fb608efd9ad7348fb
Keith Horton <khorton@microsoft.com>
2024-11-01 14:24:25 -0700

    Fixing a change that shouldn't have happened
    Signed-off-by: Keith Horton <khorton@microsoft.com>

--- src/assertions.cpp
+++ src/assertions.cpp
@@ -195,7 +195,7 @@ class AssertExtractor {
         const int offset = ins.access.offset;
         if (basereg.v == R10_STACK_POINTER) {
             // We know we are accessing the stack.
-            if (offset < -EBPF_STACK_SIZE || offset + static_cast<int>(width.v) >= 0) {
+            if (offset < -EBPF_STACK_SIZE || offset + static_cast<int>(width.v) > 0) {
                 // This assertion will fail
                 res.emplace_back(
                     ValidAccess{basereg, offset, width, false, ins.is_load ? AccessType::read : AccessType::write});
